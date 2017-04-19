98bb86e3d5feda359202ca79c180b094f879945d
Matt Rose <yamiacat@gmail.com>
2017-04-19 12:02:37 +0100

    Add start game function and next player function.

--- game.rb
+++ game.rb
@@ -1,13 +1,30 @@
 class Game
 
-  attr_reader :players
+  attr_reader :players, :players_index_position
 
   def initialize
     @players = []
+    @players_index_position = []
+
   end
 
   def add_player(player_to_be_added)
     @players << player_to_be_added
   end
+
+  def start_game
+    number_of_players = @players.count
+    until number_of_players == 0
+      number_of_players -= 1
+      @players_index_position << number_of_players
+    end
+  end
+
+  def next_player
+    @players_index_position.rotate!
+    up_next = @players_index_position.first
+    return @players[up_next]
+  end
+
 
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -9,21 +9,38 @@ class TestGame < MiniTest::Test
     @game = Game.new()
     @player1 = Player.new("Charlie")
     @player2 = Player.new("Matt")
-  end
-
-  def test_game_has_player_array
-    assert_equal([], @game.players)
-  end
-
-  def test_add_player_to_players
-    @game.add_player(@player1)
-    assert_equal([@player1], @game.players)
-  end
-
-  def test_add_multiple_player
     @game.add_player(@player1)
     @game.add_player(@player2)
+    @game.start_game
+  end
+
+  # def test_game_has_player_array
+  #   assert_equal([], @game.players)
+  # end
+  #
+  # def test_add_player_to_players
+  #   @game.add_player(@player1)
+  #   assert_equal([@player1], @game.players)
+  # end
+
+  def test_add_multiple_player
+    # @game.add_player(@player1)
+    # @game.add_player(@player2)
     assert_equal([@player1, @player2], @game.players)
   end
+
+  def test_next_player
+    assert_equal(@player1, @game.next_player)
+  end
+  #
+  # def test_next_player_two_turns
+  #   @game.next_player
+  #   assert_equal(@player2, @game.next_player)
+  # end
+
+  def test_start_game_populates_index_array
+    assert_equal(2, @game.players_index_position.count)
+  end
+
 
 end
