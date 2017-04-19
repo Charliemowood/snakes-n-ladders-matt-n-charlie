6f1b4388c27348f518dcf3253db6f58a27f54124
Matt Rose <yamiacat@gmail.com>
2017-04-19 10:40:46 +0100

    First test passed, class Pleyer created

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -4,6 +4,14 @@ require_relative("../player.rb")
 
 class TestPlayer < MiniTest::Test
 
+  def setup
+    @player1 = Player.new("Charlie")
+
+  end
+
+  def test_player_has_name
+    assert_equal("Charlie", @player1.name)
+  end
 
 
 
